eec0a9bb481db6d71398a4de5819cf470da13e7d
Arhik <arhik23@gmail.com>
2024-01-24 20:51:00 +0530

    first kernel pass by reference test

--- src/radix.jl
+++ src/radix.jl
@@ -1,21 +1,33 @@
+using Revise
 using WGPUCompute
 using WGPUCore 
 using MacroTools
 
-WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Debug)
+WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Off)
 
-x = WgpuArray{Float32}(undef, 8)
+x = WgpuArray{Float32}(rand(Float32, 32, 32) .- 0.5f0)
 
-@kernel function radixSort(x::WgpuArray{T, N}, out::WgpuArray{T, N}) where {T, N}
-	gIdx = globalId.x * globalId.y + globalId.z
-	value = x[gIdx]
-	out[gIdx] = max(value, 0.0)
+@wgpukernel workgroupSizes=(16, 16) workgroupCount=(4,) function prefixsum_kernel(x::WgpuArray{T, N}, out::WgpuArray{T, N}) where {T, N}
+	gIdx = globalId.y * numWorkGroups.x + globalId.x
+	out[gIdx] = Float32(gIdx)
 end
 
-(@macroexpand @kernel function radixSort(x::WgpuArray{T, N}) where {T, N}
-	gIdx = globalId.x * globalId.y + globalId.z
-	value = x[gIdx]
-	out[gIdx] = max(value, 0.0)
-end) |> MacroTools.striplines
+@wgpukernel workgroupSizes=(16, 16) workgroupCount=(4,) function radixSort_kernel(x::WgpuArray{T, N}, out::WgpuArray{T, N}) where {T, N}
+	gIdx = globalId.y * numWorkGroups.x + globalId.x
+	out[gIdx] = Float32(gIdx)
+end
 
-radixSort(x)
+function radixSort(x)
+	y = similar(x)
+	radixSort_kernel(x, y)
+	return y
+end
+
+function prefixsum(x)
+	y = similar(x)
+	prefixsum_kernel(x, y)
+	return y
+end
+
+y = radixSort(x)
+z = prefixsum(x)
