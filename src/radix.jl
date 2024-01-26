a1f55eb4fc945c6d40da792917b50de8f82a040e
Arhik <arhik23@gmail.com>
2024-01-26 20:18:32 +0530

    casting works and made changes to WGPUCompute

--- src/radix.jl
+++ src/radix.jl
@@ -6,16 +6,30 @@ using WGSLTypes
 using WGSLTypes: @letvar
 WGPUCore.SetLogLevel(WGPUCore.WGPULogLevel_Off)
 
-x = WgpuArray{Float32}(rand(Float32, 16, 16) .- 0.5f0)
+x = WgpuArray{Float32}(rand(Float32, 8, 8) .- 0.5f0)
 
 @wgpukernel workgroupSizes=(4, 4) workgroupCount=(2, 2) function prefixsum_kernel(x::WgpuArray{T, N}, out::WgpuArray{T, N}) where {T, N}
 	xdim = workgroupDims.x
 	ydim = workgroupDims.y
 	gIdx = workgroupId.x*xdim + localId.x
 	gIdy = workgroupId.y*ydim + localId.y
-	dim = UInt32(16)
-	gId = dim*gIdy + gIdx
-	out[gId] = Float32(gId)
+	gId = xDims.x*gIdy + gIdx
+	out[gId] = T(gId)
+end
+
+@wgpukernel workgroupSizes=(4, 4) workgroupCount=(2, 2) function cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
+	xdim = workgroupDims.x
+	ydim = workgroupDims.y
+	gIdx = workgroupId.x*xdim + localId.x
+	gIdy = workgroupId.y*ydim + localId.y
+	gId = xDims.x*gIdy + gIdx
+	out[gId] = S(ceil(x[gId]))
+end
+
+function cast(S::DataType, x::WgpuArray{T, N}) where {T, N}
+	y = WgpuArray{S}(undef, size(x))
+	cast_kernel(x, y)
+	return y
 end
 
 function prefixsum(x)
@@ -25,4 +39,7 @@ function prefixsum(x)
 end
 
 z = prefixsum(x)
+z = cast(UInt32, x)
 
+# TODO
+# y = cast(Bool, x)
